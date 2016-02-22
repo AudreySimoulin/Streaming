964a11689a4f7aec49d7a03c379a8a0aab7481db
AudreySimoulin <a.simoulin@gmail.com>
2016-02-22 14:45:09 +0100

    blabla

--- src/streaming/entity/Film.java
+++ src/streaming/entity/Film.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-Entité gérant les films
+Entité gérant les films 
  */
 package streaming.entity;
 
