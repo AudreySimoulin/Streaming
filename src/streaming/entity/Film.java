a790d02182e6105456eb177d721cafc3482aee8d
aaventin <agathe.aventin@soprasteria.com>
2016-02-22 15:29:32 +0100

    Film et Genre modifiés

--- src/streaming/entity/Film.java
+++ src/streaming/entity/Film.java
@@ -1,4 +1,5 @@
 /*
+Je suis passée par ici ^^
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
